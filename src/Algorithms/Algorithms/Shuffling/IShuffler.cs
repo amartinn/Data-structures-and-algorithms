e764cecc471ed888b4e056a19b3fdfb872b94259
=amartinn <martoy123691@gmail.com>
2021-01-18 00:50:21 +0200

    feature: Fisher Yates

---
+++ src/Algorithms/Algorithms/Shuffling/IShuffler.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms.Shuffling
+{
+    /// <summary>
+    /// Shuffling an array of type <typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T">Type of the array element.</typeparam>
+    public interface IShuffler<T>
+    {
+        /// <summary>
+        /// shuffle an array of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="array">Array to shuffle.</param>
+        void Shuffle(T[] array);
+    }
+}
