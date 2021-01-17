e363438f6f296486795f3b622a2e03f163728d09
=amartinn <martoy123691@gmail.com>
2021-01-18 01:34:04 +0200

    fix: using statements

--- src/Algorithms/Algorithms/Sorting/Comparisson/QuickSorter.cs
+++ src/Algorithms/Algorithms/Sorting/Comparisson/QuickSorter.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Algorithms.Sorting.Comparisson
+﻿namespace Algorithms.Sorting.Comparisson
 {
+    using System;
+    using System.Collections.Generic;
     /// <summary>
     /// Class Implementing quick sort algorithm.
     /// </summary>
