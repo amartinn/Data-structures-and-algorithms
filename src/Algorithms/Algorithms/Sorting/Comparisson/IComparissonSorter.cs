c355de2d13b75e061834b1fb79ebbbf73a72020e
=amartinn <martoy123691@gmail.com>
2021-01-17 22:58:51 +0200

    initial commit

---
+++ src/Algorithms/Algorithms/Sorting/Comparisson/IComparissonSorter.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Sorting.Comparisson
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Sorting an array in ascending order <typeparamref name="T"/> using comparisson sort.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IComparissonSorter<T>
+    {
+        /// <summary>
+        /// Sorts an array of <typeparamref name="T"/> using a comparer in ascending order.
+        /// </summary>
+        /// <param name="array">The array to sort</param>
+        /// <param name="comparer">Comparer to compare elements of <paramref name="array"/></param>
+        void Sort(T[] array, IComparer<T> comparer);
+    }
+}
---
+++ src/Algorithms/Algorithms/Sorting/Integer/IIntegerSorter.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms.Sorting.Integer
+{
+    /// <summary>
+    /// Sorts an array of integers
+    /// </summary>
+    public interface IIntegerSorter
+    {
+        /// <summary>
+        /// Sorts integer array in ascending order
+        /// </summary>
+        /// <param name="array">The Array to sort</param>
+        void Sort(int[] array);
+    }
+}
